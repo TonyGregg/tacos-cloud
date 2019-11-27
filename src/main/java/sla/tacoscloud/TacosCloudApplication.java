9750827bf5ab351738c793447a1a2c0c18242ed0
TonyGregg <antonygenil@gmail.com>
2019-11-26 20:08:05 -0600

    Initial commit

---
+++ src/main/java/sla/tacoscloud/TacosCloudApplication.java
@@ -0,0 +1,14 @@
+package sla.tacoscloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacosCloudApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TacosCloudApplication.class, args);
+	}
+
+}
---
+++ src/main/java/sla/tacoscloud/controller/TacoController.java
@@ -0,0 +1,7 @@
+package sla.tacoscloud.controller;
+
+/**
+ * Created by Antony Genil on 2019-11-26 at 20 04 for tacos-cloud
+ **/
+public class TacoController {
+}
